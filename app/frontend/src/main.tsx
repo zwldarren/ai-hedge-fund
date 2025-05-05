b1297070f14627ddd71380920ad161458bcc5b81
virattt <virat.dot@gmail.com>
2025-05-05 08:29:27 -0400

    Add tailwindcss

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { ThemeProvider } from './contexts/theme-provider';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
