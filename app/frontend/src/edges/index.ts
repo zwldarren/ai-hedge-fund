00061574448befd7242e516f0207cef4685ad5c8
virattt <virat.dot@gmail.com>
2025-05-07 08:32:39 -0400

    Do not animate edges

--- app/frontend/src/edges/index.ts
+++ app/frontend/src/edges/index.ts
@@ -1,8 +1,8 @@
 import type { Edge, EdgeTypes } from '@xyflow/react';
 
 export const initialEdges: Edge[] = [
-  { id: 'a->b', source: 'a', target: 'b', animated: true },
-  { id: 'b->c', source: 'b', target: 'c', animated: true },
+  { id: 'a->b', source: 'a', target: 'b', animated: false },
+  { id: 'b->c', source: 'b', target: 'c', animated: false },
 ];
 
 export const edgeTypes = {
