aaf50e5186d1699f9786f5820f76672a16f5a346
virattt <virat.dot@gmail.com>
2025-05-09 17:08:16 -0400

    Add NodeMessage

--- app/frontend/src/nodes/types.ts
+++ app/frontend/src/nodes/types.ts
@@ -1,4 +1,7 @@
+import { MessageItem } from '@/contexts/node-context';
 import type { BuiltInNode, Node } from '@xyflow/react';
+
+export type NodeMessage = MessageItem;
 
 export type AgentNode = Node<{ name: string, description: string, status: string }, 'agent'>;
 export type StartNode = Node<{ name: string, description: string, status: string }, 'start'>;
