993ad42b1c17431b379c49e56eb39a11f9da7849
virattt <virat.dot@gmail.com>
2025-05-12 17:30:56 -0400

    Add initial edges

--- app/frontend/src/nodes/index.ts
+++ app/frontend/src/nodes/index.ts
@@ -1,4 +1,4 @@
-import { type NodeTypes } from '@xyflow/react';
+import { type NodeTypes, Edge, MarkerType } from '@xyflow/react';
 
 import { AgentNode } from './components/agent-node';
 import { StartNode } from './components/start-node';
@@ -18,34 +18,40 @@ export const initialNodes: AppNode[] = [
       status: 'Idle',
     },
   },
+];
+
+// TODO (virat) temporary, delete after testing
+export const initialEdges: Edge[] = [
   {
-    id: 'warren_buffett',
-    type: 'agent',
-    position: { x: 450, y: -150 },
-    data: {
-      name: 'Warren Buffett',
-      description: 'The Oracle of Omaha',
-      status: 'Idle'
+    id: 'start-to-warren',
+    source: 'start',
+    target: 'warren_buffett',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
     },
   },
   {
-    id: 'charlie_munger',
-    type: 'agent',
-    position: { x: 450, y: 150 },
-    data: {
-      name: 'Charlie Munger',
-      description: 'The Abominable No-Man',
-      status: 'Idle'
+    id: 'start-to-charlie',
+    source: 'start',
+    target: 'charlie_munger',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
     },
   },
   {
-    id: 'portfolio_manager',
-    type: 'agent',
-    position: { x: 900, y: 0 },
-    data: {
-      name: 'Portfolio Manager',
-      description: 'Decision Maker',
-      status: 'Idle'
+    id: 'warren-to-portfolio',
+    source: 'warren_buffett',
+    target: 'portfolio_manager',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  },
+  {
+    id: 'charlie-to-portfolio',
+    source: 'charlie_munger',
+    target: 'portfolio_manager',
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
     },
   },
 ];
