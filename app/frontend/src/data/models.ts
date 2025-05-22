db22c63fbc0a7dd395e821ae4d4a03893b49802b
virattt <virat.dot@gmail.com>
2025-05-22 17:08:40 -0400

    Add Claude 4 models

--- app/frontend/src/data/models.ts
+++ app/frontend/src/data/models.ts
@@ -6,52 +6,57 @@ export interface ModelItem {
 
 export const apiModels: ModelItem[] = [
   {
-    "display_name": "claude-3.5-haiku",
+    "display_name": "Claude Haiku 3.5",
     "model_name": "claude-3-5-haiku-latest",
     "provider": "Anthropic"
   },
   {
-    "display_name": "claude-3.7-sonnet",
-    "model_name": "claude-3-7-sonnet-latest",
+    "display_name": "Claude Sonnet 4",
+    "model_name": "claude-sonnet-4-20250514",
     "provider": "Anthropic"
   },
   {
-    "display_name": "deepseek-r1",
+    "display_name": "Claude Opus 4",
+    "model_name": "claude-opus-4-20250514",
+    "provider": "Anthropic"
+  },
+  {
+    "display_name": "DeepSeek R1",
     "model_name": "deepseek-reasoner",
     "provider": "DeepSeek"
   },
   {
-    "display_name": "deepseek-v3",
+    "display_name": "DeepSeek V3",
     "model_name": "deepseek-chat",
     "provider": "DeepSeek"
   },
   {
-    "display_name": "gemini-2.5-flash",
+    "display_name": "Gemini 2.5 Flash",
     "model_name": "gemini-2.5-flash-preview-05-20",
     "provider": "Gemini"
   },
   {
-    "display_name": "gemini-2.5-pro",
+    "display_name": "Gemini 2.5 Pro",
     "model_name": "gemini-2.5-pro-exp-03-25",
     "provider": "Gemini"
   },
   {
-    "display_name": "llama-4-scout-17b",
+    "display_name": "Llama 4 Scout (17b)",
     "model_name": "meta-llama/llama-4-scout-17b-16e-instruct",
     "provider": "Groq"
   },
   {
-    "display_name": "llama-4-maverick-17b",
+    "display_name": "Llama 4 Maverick (17b)",
     "model_name": "meta-llama/llama-4-maverick-17b-128e-instruct",
     "provider": "Groq"
   },
   {
-    "display_name": "gpt-4.5",
+    "display_name": "GPT 4.5",
     "model_name": "gpt-4.5-preview",
     "provider": "OpenAI"
   },
   {
-    "display_name": "gpt-4o",
+    "display_name": "GPT 4o",
     "model_name": "gpt-4o",
     "provider": "OpenAI"
   },
@@ -61,7 +66,7 @@ export const apiModels: ModelItem[] = [
     "provider": "OpenAI"
   },
   {
-    "display_name": "o4-mini",
+    "display_name": "o4 Mini",
     "model_name": "o4-mini",
     "provider": "OpenAI"
   }
