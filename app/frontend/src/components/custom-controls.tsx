3f2f4a03b2c206df084309ba34ceb1eee15acfe0
virattt <virat.dot@gmail.com>
2025-06-20 15:10:01 -0400

    Fix runtime warning

--- app/frontend/src/components/custom-controls.tsx
+++ app/frontend/src/components/custom-controls.tsx
@@ -1,6 +1,5 @@
 import { ResetIcon } from '@radix-ui/react-icons';
 import { ControlButton, Controls } from '@xyflow/react';
-import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
 type CustomControlsProps = {
   onReset: () => void;
@@ -14,16 +13,9 @@ export function CustomControls({ onReset }: CustomControlsProps) {
       style={{ bottom: 20, borderRadius: 20, gap: 10 }}
       className="bg-ramp-grey-800 text-white px-4 py-2 rounded-md [&_button]:border-0 [&_button]:outline-0 [&_button]:shadow-none"
     >
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <ControlButton onClick={onReset} title="Reset Flow">
-              <ResetIcon />
-            </ControlButton>
-          </TooltipTrigger>
-          <TooltipContent side="top" className="z-[9999]">
-            <p>Reset Flow</p>
-          </TooltipContent>
-        </Tooltip>
+        <ControlButton onClick={onReset} title="Reset Flow">
+          <ResetIcon />
+        </ControlButton>
     </Controls>
   );
 } 
